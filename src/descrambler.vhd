fe0952fb91498ccb2fac534ecac822f498166825
David R. <davidrubio97@outlook.com>
2024-06-25 16:38:34 +0200

    update

--- src/descrambler.vhd
+++ src/descrambler.vhd
@@ -1,43 +1,43 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/25/2024 10:41:51 AM
--- Design Name: 
--- Module Name: descrambler - arch_descrambler
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity descrambler is
---  Port ( );
+    Port ( 
+        clk : in std_logic;
+        rst_n : in std_logic;
+        data_in : in std_logic_vector(15 downto 0);
+        data_out : out std_logic_vector(15 downto 0)
+    );
 end descrambler;
 
 architecture arch_descrambler of descrambler is
 
+signal data_out_aux : std_logic_vector(15 downto 0);
+signal first_epoch : std_logic_vector(15 downto 0);
+signal dff_aux : std_logic_vector(14 downto 0);
+
 begin
 
+    data_out <= data_out_aux;
+    
+impl_data_out : for i in 0 to 15 generate
+        data_out_aux(i) <= data_in(i) xor first_epoch(i);
+    end generate;
+
+    first_epoch(0) <= data_in(15) xor data_in(14);
+    first_epoch(1) <= data_in(5) xor dff_aux(0);
+    
+impl_first_epoch : for j in 2 to 15 generate
+        first_epoch(j) <= dff_aux(j-2) xor dff_aux(j-1);
+    end generate;
+    
+    process(clk, rst_n)
+    begin
+        if rst_n = '0' then
+            dff_aux <= (others=>'0');
+        elsif rising_edge(clk) then
+            dff_aux <= data_in(dff_aux'length-1 downto 0);
+        end if;
+    end process;
 
 end arch_descrambler;
--- src/scrambler.vhd
+++ src/scrambler.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
 
 entity scrambler is
     Port ( 
@@ -14,12 +13,37 @@ end scrambler;
 architecture arch_scrambler of scrambler is
 
 signal scrambler : std_logic_vector(15 downto 0);
+signal first_epoch : std_logic_vector(15 downto 0);
+signal dff_aux : std_logic_vector(14 downto 0);
+signal data_out_aux : std_logic_vector(15 downto 0);
 
 begin
 
---    scrambler <= data_in(15) xor data_in(14);
+
+    data_out <= data_out_aux;
 
 
-    data_out <= data_in xor scrambler;
+        first_epoch(0) <= data_in(0) xor data_out_aux(14);
+        first_epoch(1) <= data_in(1) xor data_out_aux(15);
+    
+    impl_first_epoch : for i in 2 to 15 generate
+            first_epoch(i) <= data_in(i) xor dff_aux(i-2);
+        end generate;
+        
+        data_out_aux(0) <= first_epoch(0) xor data_out_aux(15);
+       
+    impl_second_epoch : for j in 1 to 15 generate
+            data_out_aux(j) <= first_epoch(j) xor dff_aux(j-1);
+        end generate;
+        
+    
+    process(clk, rst_n)
+    begin
+        if rst_n = '0' then
+            dff_aux <= (others=>'0');
+        elsif rising_edge(clk) then
+            dff_aux <= data_out_aux(dff_aux'length-1 downto 0);
+        end if;
+    end process;
 
 end arch_scrambler;
